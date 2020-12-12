3230f716ccd32b20ba97b02dafd80ac2a648a844
Mahtab Sabet <mahtabz@gmail.com>
2020-12-12 15:02:34 +0000

    Pull the datapoints out into a separate file as a constant

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react'
+import React from 'react'
 import CanvasJSReact from '../assets/canvasjs.stock.react';
-var CanvasJS = CanvasJSReact.CanvasJS;
+import {shipnoiseData} from '../constants/shipnoiseData';
 var CanvasJSStockChart = CanvasJSReact.CanvasJSStockChart;
-
-var dataPoints = [];
 
 export default class Graph extends React.Component {
     render() {
+      //TODO: fetch from API instead of const
+    var dataPoints = shipnoiseData;
     const options = {
       title: {
           text: "ShipNoise Graph"
@@ -15,33 +15,7 @@ export default class Graph extends React.Component {
           data: [{
             type: "line",
             toolTipContent: "<b>Ship Name:</b> {name} <br/> <b>MMSI:</b> {mmsi} <hr/> <b>Noise:</b> {y}dB ",
-            //TODO: pull this from the API
-            dataPoints: [
-	      { x: new Date("2018-01-01Z00:00"), y: 71, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:01"), y: 55, name: "Boaty McBoatFace", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:02"), y: 50, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:03"), y: 65, name: "Seas the Day", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:04"), y: 95, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:05"), y: 68, name: "Boaty McBoatFace", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:06"), y: 28, name: "HMS Pinafore", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:07"), y: 34, name: "Seas the Day", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:08"), y: 14, name: "HMS Pinafore", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:09"), y: 71, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:10"), y: 55, name: "Boaty McBoatFace", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:11"), y: 50, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:12"), y: 34, name: "HMS Pinafore", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:13"), y: 50, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:14"), y: 50, name: "Seas the Day", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:15"), y: 95, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:16"), y: 68, name: "HMS Pinafore", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:17"), y: 28, name: "Boaty McBoatFace", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:18"), y: 34, name: "HMS Pinafore", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:19"), y: 65, name: "Seas the Day", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:20"), y: 55, name: "Boaty McBoatFace", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:21"), y: 71, name: "HMS Pinafore", mmsi: 12345 },
-	      { x: new Date("2018-01-01Z00:22"), y: 55, name: "Seas the Day", mmsi: 6789 },
-	      { x: new Date("2018-01-01Z00:23"), y: 50, name: "HMS Pinafore", mmsi: 12345 }
-	  ]
+            dataPoints: dataPoints
          }]
       }],
       navigator: {
---
+++ src/constants/shipnoiseData.js
@@ -0,0 +1,28 @@
+//TODO: This is being stored here as a constant for now, but eventually we should be fetching from the API instead
+export const shipnoiseData = 
+    [
+        { x: new Date("2018-01-01Z00:00"), y: 71, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:01"), y: 55, name: "Boaty McBoatFace", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:02"), y: 50, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:03"), y: 65, name: "Seas the Day", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:04"), y: 95, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:05"), y: 68, name: "Boaty McBoatFace", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:06"), y: 28, name: "HMS Pinafore", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:07"), y: 34, name: "Seas the Day", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:08"), y: 14, name: "HMS Pinafore", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:09"), y: 71, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:10"), y: 55, name: "Boaty McBoatFace", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:11"), y: 50, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:12"), y: 34, name: "HMS Pinafore", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:13"), y: 50, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:14"), y: 50, name: "Seas the Day", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:15"), y: 95, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:16"), y: 68, name: "HMS Pinafore", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:17"), y: 28, name: "Boaty McBoatFace", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:18"), y: 34, name: "HMS Pinafore", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:19"), y: 65, name: "Seas the Day", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:20"), y: 55, name: "Boaty McBoatFace", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:21"), y: 71, name: "HMS Pinafore", mmsi: 12345 },
+        { x: new Date("2018-01-01Z00:22"), y: 55, name: "Seas the Day", mmsi: 6789 },
+        { x: new Date("2018-01-01Z00:23"), y: 50, name: "HMS Pinafore", mmsi: 12345 }
+    ];
